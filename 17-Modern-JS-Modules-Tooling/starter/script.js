fe51ff56b1f69d46d735d96cba237e3887a1ec3b
JosephiusX <josephiusx@gmail.com>
2021-03-25 17:17:01 -0700

    saving to be safe

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,4 @@
+// Importing moduld
+// all import statements are hoisted to the top
+import './shoppingCart.js' // es6 knows its a .js file so the extention isint really nessessary
+console.log("importing module");
